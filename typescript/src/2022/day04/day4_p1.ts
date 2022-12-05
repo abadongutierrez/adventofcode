171e6b675e2156d69ce106683a2d0d26e2c1f72a
Rafael Gutierrez <abadon.gutierrez@gmail.com>
2022-12-05 16:36:40 -0600

    day 4 solution

---
+++ typescript/src/2022/day04/day4_p1.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import {join} from "path";
+
+const fileName = 'input.txt'
+const result = fs.readFileSync(join(__dirname, fileName), 'utf-8');
+const lines = result.split(/[\n]/);
+
+function extractRange(line: string, rangeStr: string) {
+    const numbersStr = rangeStr.split('-')
+    return [parseInt(numbersStr[0]), parseInt(numbersStr[1])];
+}
+
+function containsRage(range1: number[], range2: number[]) {
+    return range2[0] >= range1[0] && range2[1] <= range1[1];
+}
+
+let containCount = 0;
+
+for (const i in lines) {
+    const line = lines[i];
+    const range1 = extractRange(line, line.split(',')[0]);
+    const range2 = extractRange(line, line.split(',')[1]);
+    containCount += (containsRage(range1, range2) || containsRage(range2, range1) ? 1 : 0);
+}
+
+console.log(`${containCount}`);
