74038dd9b089885b1da7e9cb26079306fd027483
Rafael Gutiérrez <abadon.gutierrez@gmail.com>
2024-12-12 23:16:56 -0600

    solution for day 6-part1 and part2, 2024

--- java/src/main/java/com/jabaddon/adventofcode/_2024/day06/Solution.java
+++ java/src/main/java/com/jabaddon/adventofcode/_2024/day06/Solution.java
@@ -3,71 +3,112 @@ package com.jabaddon.adventofcode._2024.day06;
 import com.jabaddon.adventofcode.InputReader;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         List<String> lines = InputReader.readLines(Solution.class, "input.txt");
 
         var solution = new Solution();
-        System.out.println(solution.part1(lines));
-        // TODO part2
+        System.out.println(solution.part1(lines).size());
+        System.out.println(solution.part2(lines));
     }
 
-    private int part1(List<String> original) {
+    private int part2(List<String> original) {
+        Set<Position> visitedPositions = part1(original);
         List<String> lines = new ArrayList<>(original);
-        int totalRows = lines.size();
-        int totalCols = lines.get(0).length();
-        GuardPositionAndDirection current = findGuard(lines);
-        Set<Position> visitedPositions = new HashSet<>();
-        int countDistinctPositions = 0;
+        GuardPositionAndDirection initPosition = findGuard(lines);
+        // remove guard current position
+        visitedPositions.remove(new Position(initPosition.row(), initPosition.col()));
+        Set<Position> obstacles = findObstacles(lines);
+        AtomicInteger count = new AtomicInteger(0);
+        visitedPositions.forEach(vistedPosition -> {
+            try {
+                Set<Position> newObstacles = new HashSet<>(obstacles);
+                newObstacles.add(vistedPosition);
+                internalGetPositions(initPosition, lines, newObstacles);
+            } catch (RuntimeException ex) {
+                if (ex.getMessage().equals("Cycle!")) count.incrementAndGet();
+            }
+        });
+        return count.get();
+    }
+
+    private Set<Position> part1(List<String> original) {
+        List<String> lines = new ArrayList<>(original);
+        Set<Position> obstacles = findObstacles(lines);
+        GuardPositionAndDirection last = findGuard(lines);
+        return internalGetPositions(last, lines, obstacles);
+    }
+
+    private Set<Position> internalGetPositions(GuardPositionAndDirection last, List<String> lines, Set<Position> obstacles) {
+        Map<Position, Integer> visitedPositions = new HashMap<>();
         // follow guard direction until is out of the map
         while (true) {
-//            System.out.println(current);
-            GuardPositionAndDirection next = null;
-            switch (current.direction()) {
-                case '^' -> next = new GuardPositionAndDirection(current.row() - 1, current.col(), current.direction());
-                case 'v' -> next = new GuardPositionAndDirection(current.row() + 1, current.col(), current.direction());
-                case '<' -> next = new GuardPositionAndDirection(current.row(), current.col() - 1, current.direction());
-                case '>' -> next = new GuardPositionAndDirection(current.row(), current.col() + 1, current.direction());
-            }
-            // verify if guard is still inside the map
-            if (next.row() >= 0 && next.row() < totalRows && next.col() >= 0 && next.col() < totalCols) {
-                // verify if new guard position hit an obstacle
-                if (lines.get(next.row()).charAt(next.col()) == '#') {
-                    var newDir = ' ';
-                    switch (next.direction()) {
-                        case '^' -> newDir = '>';
-                        case 'v' -> newDir = '<';
-                        case '<' -> newDir = '^';
-                        case '>' -> newDir = 'v';
-                    }
-                    // new guard position is the last one but with new direction
-                    next = new GuardPositionAndDirection(current.row(), current.col(), newDir);
+            GuardPositionAndDirection next = getNextPositionKeepingDirection(last);
+            if (next.position().isInsideMap(lines)) {
+                boolean hitObstacle = obstacles.contains(next.position());
+                if (hitObstacle) {
+                    next = new GuardPositionAndDirection(
+                            last.row(), last.col(), getNewDirection(next.direction()));
                 }
-                // update visited position in map
-                countDistinctPositions = getCountDistinctPositions(current, visitedPositions, countDistinctPositions);
-                // update guard position
-                current = next;
+                addDistinctPositionIfApply(last, visitedPositions, hitObstacle);
+                // TODO: find a better way to find a cycle!!!
+                if (hitObstacle && visitedPositions.get(last.position()) > 100) {
+                    throw new RuntimeException("Cycle!");
+                }
+                last = next;
             } else {
-                // guard is out of the map
-                countDistinctPositions = getCountDistinctPositions(current, visitedPositions, countDistinctPositions);
+                addDistinctPositionIfApply(last, visitedPositions, false);
                 break;
             }
         }
-        return countDistinctPositions;
+        return visitedPositions.keySet();
     }
 
-    private int getCountDistinctPositions(GuardPositionAndDirection current, Set<Position> visitedPositions, int count) {
-        var pos = new Position(current.row(), current.col());
-        if (!visitedPositions.contains(pos)) {
-            visitedPositions.add(pos);
-            return count + 1;
+    private Set<Position> findObstacles(List<String> lines) {
+        Set<Position> obstacles = new HashSet<>();
+        for (int row = 0; row < lines.size(); row++) {
+            for (int col = 0; col < lines.get(row).length(); col++) {
+                char c = lines.get(row).charAt(col);
+                if (c == '#') {
+                    obstacles.add(new Position(row, col));
+                }
+            }
         }
-        return count;
+        return obstacles;
+    }
+
+    private char getNewDirection(char direction) {
+        var newDir = direction;
+        switch (direction) {
+            case '^' -> newDir = '>';
+            case 'v' -> newDir = '<';
+            case '<' -> newDir = '^';
+            case '>' -> newDir = 'v';
+        }
+        return newDir;
+    }
+
+    private GuardPositionAndDirection getNextPositionKeepingDirection(GuardPositionAndDirection current) {
+        GuardPositionAndDirection next = null;
+        switch (current.direction()) {
+            case '^' -> next = new GuardPositionAndDirection(current.row() - 1, current.col(),  current.direction());
+            case 'v' -> next = new GuardPositionAndDirection(current.row() + 1, current.col(), current.direction());
+            case '<' -> next = new GuardPositionAndDirection(current.row(), current.col() - 1, current.direction());
+            case '>' -> next = new GuardPositionAndDirection(current.row(), current.col() + 1, current.direction());
+        }
+        return next;
+    }
+
+    private void addDistinctPositionIfApply(GuardPositionAndDirection current, Map<Position, Integer> visitedPositions, boolean hit) {
+        var pos = new Position(current.row(), current.col());
+        if (!visitedPositions.containsKey(pos)) {
+            visitedPositions.put(pos, 0);
+        } else if (hit) {
+            visitedPositions.put(pos, visitedPositions.get(pos) + 1);
+        }
     }
 
     private GuardPositionAndDirection findGuard(List<String> lines) {
@@ -84,7 +125,13 @@ public class Solution {
 }
 
 record Position(int row, int col) {
+    boolean isInsideMap(List<String> lines) {
+        return row() >= 0 && row() < lines.size() && col() >= 0 && col() < lines.get(0).length();
+    }
 }
 
 record GuardPositionAndDirection(int row, int col, char direction) {
+    public Position position() {
+        return new Position(row, col);
+    }
 }
